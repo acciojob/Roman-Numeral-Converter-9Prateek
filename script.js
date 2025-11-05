8d07d56476e8acbfe2b4e54bc5e929c70839d876
prateek <pratikkumar09835@gmail.com>
2025-11-05 15:36:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+function integerToRoman(num) {
+  if (typeof num !== 'number' || num <= 0 || num >= 4000) {
+    throw new Error('Input must be an integer between 1 and 3999');
+  }
 
-  //your code here
+  const values  = [1000, 900, 500, 400, 100,  90,   50,   40,   10,   9,    5,   4,    1];
+  const symbols = ['M',  'CM', 'D',  'CD', 'C', 'XC', 'L',  'XL',  'X',  'IX', 'V', 'IV', 'I'];
 
+  let roman = '';
+  for (let i = 0; i < values.length; i++) {
+    while (num >= values[i]) {
+      roman += symbols[i];
+      num   -= values[i];
+    }
+  }
+  return roman;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+// Examples:
+console.log(integerToRoman(14)); 
+console.log(integerToRoman(1994)); 
+console.log(integerToRoman(3));   
